0b028f64f8781e526ec19bbf668897638d6540bc
VIctor <victorguedesferreira@yahoo.com.br>
2020-10-04 00:21:25 -0300

    Testando commits

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Primeiro Commit
         </a>
       </header>
     </div>
